a3b3fcfed39ee90e3505a36d377967098a7f3913
ongoing-z <code@ongoingblog.org>
2025-08-05 14:16:59 +0800

    去除废弃引用。

--- seata-spring-boot-starter/src/test/java/simple_seata_demo_01/service/AccountService.java
+++ seata-spring-boot-starter/src/test/java/simple_seata_demo_01/service/AccountService.java
@@ -2,11 +2,8 @@ package simple_seata_demo_01.service;
 
 import org.apache.seata.core.context.RootContext;
 import org.apache.seata.spring.annotation.GlobalTransactional;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.sql.DataSource;
 
 @Service
 public class AccountService {
